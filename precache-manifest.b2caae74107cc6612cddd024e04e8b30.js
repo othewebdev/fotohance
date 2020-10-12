bb0bbedf0ab4db8cd0b2020b9f3af3c5e43a51fe
Odis Barnett <kingo.iv@gmail.com>
2020-10-11 20:58:08 -0500

    Updates

---
+++ precache-manifest.b2caae74107cc6612cddd024e04e8b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35dfee732e5897528339086a1a9501e7",
+    "url": "/fotohancer/index.html"
+  },
+  {
+    "revision": "280f994afe0a330004d7",
+    "url": "/fotohancer/static/css/main.2eba23c6.chunk.css"
+  },
+  {
+    "revision": "09c34bd94ee16a63c4b6",
+    "url": "/fotohancer/static/js/2.a786c6b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fotohancer/static/js/2.a786c6b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280f994afe0a330004d7",
+    "url": "/fotohancer/static/js/main.f8d3b074.chunk.js"
+  },
+  {
+    "revision": "eb66a1586ed0028e340a",
+    "url": "/fotohancer/static/js/runtime-main.562b8490.js"
+  }
+]);
